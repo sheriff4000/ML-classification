4bb7ec08f33823c76027962a4c2df78b1a0c4342
sheriff4000 <sheriff4000@gmail.com>
2022-10-21 17:10:16 +0100

    read_file

--- CW1.py
+++ CW1.py
@@ -1,13 +1,13 @@
 import numpy as np
 
+def read_file(file):
+    x = []
+    y_labels = []
+    with open(file, "r") as infile:
+        for i, line in enumerate(infile):
+            row = line.split()
+            x.append(list(map(float, row[:-1])))
+            y_labels.append(row[-1])
 
-x = []
-y_labels = []
-with open("intro2ML-coursework1/wifi_db/clean_dataset.txt", "r") as infile:
-    for i, line in enumerate(infile):
-        row = line.split()
-        x.append(list(map(float, row[:-1])))
-        y_labels.append(row[-1])
-
-    x = np.array(x)
-    y = np.array(y)
+        x = np.array(x)
+        y = np.array(y)
